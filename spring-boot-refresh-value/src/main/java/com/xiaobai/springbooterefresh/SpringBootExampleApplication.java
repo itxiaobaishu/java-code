9b1f739728cd3c7c4ebff24e71826f5ed7187017
王丹宇 <wangdanyu@zhichubao.com>
2024-02-19 14:22:00 +0800

    feat: 动态刷新@Value字段值

---
+++ spring-boot-refresh-value/src/main/java/com/xiaobai/springbooterefresh/SpringBootExampleApplication.java
@@ -0,0 +1,17 @@
+package com.xiaobai.springbooterefresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description
+ *
+ * @author mac 2024/02/19 11:37
+ */
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExampleApplication.class, args);
+    }
+}
